c5ae2b6422d51c97c3805bbfc06ce5c1c76e9fbc
Alistair <gillaw06@gmail.com>
2013-05-04 11:11:32 +0800

    Upload of imageupload ruby application
    Authlogic is almost implemented. Its a gem for authentication and
    management of user sessions. A few issues need to be fixed. i.e.
    routing using the map method
    
    tutorial for an idea of how it works:
    https://github.com/binarylogic/authlogic_example

---
+++ imageupload/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Imageupload::Application.config.secret_token = '02b5d5d446da3babfde06d4140bcc91ecf50095db9cafae431ff5dc507f4edb69c020ed564e0296ec6ca33ba6c8d3b857dc0e18ae7c917eb9b62182bd70a334f'
