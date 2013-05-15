bad3e470d23aff499e38ed5d38c3cda628ca2870
Alistair <gillaw06@gmail.com>
2013-05-15 12:15:12 +0800

    Profile page clean up

--- imageupload/app/uploaders/image_uploader.rb
+++ imageupload/app/uploaders/image_uploader.rb
@@ -28,7 +28,7 @@ class ImageUploader < CarrierWave::Uploader::Base
   #   "/images/fallback/" + [version_name, "default.png"].compact.join('_')
   # end
   version :thumb do
-      process :resize_to_limit => [200, 200]
+      process :resize_to_fill => [200, 200]
     end
   # Process files as they are uploaded:
   # process :scale => [200, 300]
