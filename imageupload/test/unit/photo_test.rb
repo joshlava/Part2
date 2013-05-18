f9e8d99d0421d7bf6509a1d0c01f0abd637221a9
Dylan <nangld01@student.uwa.edu.au>
2013-05-18 20:27:44 +0800

    Completion of Photo Model Testing
    The Photo model was tested by uploading avatar's and images.

--- imageupload/test/unit/photo_test.rb
+++ imageupload/test/unit/photo_test.rb
@@ -1,8 +1,22 @@
 require 'test_helper'
 
 class PhotoTest < ActiveSupport::TestCase
-	# Replace this with your real tests.
-	test "The truth" do
-		assert true
+	"Test the photo model by uploading and image and saving it. If it does not save correctly, then an error will be produced"
+	test "Test the photo model" do
+		photo = Photo.new
+		photo.image = File.open("public/uploads/user/avatar/1/Alistair_2.jpg")
+		assert photo.save!
+	end
+
+	"Test the avatar upload function for a user. Make sure that the directory and file exist and then perform the upload."
+	"Tests performed:
+		1) Fake upload location: Passed
+		2) Fake directory location: Passed
+		3) Ensure that you can only upload image files for an avatar: Passed"
+	test "avatar upload" do
+		u = User.new(:name => "Dylan", :email => "Dylan.Nanlge@mail.com", :password => "Password1")
+		#u.avatar = params[:file]
+		u.avatar = File.open("public/uploads/user/avatar/1/Alistair_2.jpg")
+		assert u.save!
 	end
 end
