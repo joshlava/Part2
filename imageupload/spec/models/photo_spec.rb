182ee6a5fd27ec74d8a8baedfa6e9f2b17e43cdf
Dylan <nangld01@student.uwa.edu.au>
2013-05-19 02:26:23 +0800

    Creation of Photo Model Tests
    Having issues trying to test the upload section of the website. Looks
    like we need to pass an uploaded model directly to the model.

--- imageupload/spec/models/photo_spec.rb
+++ imageupload/spec/models/photo_spec.rb
@@ -1,5 +1,23 @@
 require 'spec_helper'
 
 describe Photo do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  before do
+    @user = User.new(name: "Dylan1", email: "Dylan.Nangle@gmail1.com",
+                     password: "Password1")
+
+    test_image = "test/fixtures/Unknown.jpg"
+    file = Rack::Test::UploadedFile.new(test_image, "image/jpeg")
+    @photo = Photo.new(user_id: 1, image: file)
+  end
+
+  subject { @photo }
+
+  describe "when no file is selected" do
+    before { @photo.image = "" }
+    it { should_not be_valid }
+  end
+
+    
+   
 end
