dba4a706d197da31ca685d723390aeaefe4011a3
Dylan <nangld01@student.uwa.edu.au>
2013-05-18 21:42:36 +0800

    Completion of ability and post models
    Simple tests as they are just checking for validation

--- imageupload/test/unit/ability_test.rb
+++ imageupload/test/unit/ability_test.rb
@@ -1,1 +1,10 @@
+require 'test_helper'
 
+class AbilityTest < ActiveSupport::TestCase
+	test "Create new user" do
+		user = User.new(:name => "Dylan", :email => "dylan.nangle@gmail.com")
+		if user.has_role? :user 
+			assert (true)
+		end
+	end
+end
--- imageupload/test/unit/post_test.rb
+++ imageupload/test/unit/post_test.rb
@@ -1,8 +1,16 @@
 require 'test_helper'
 
 class PostTest < ActiveSupport::TestCase
-	# Replace this with your real tests.
-	test "The truth" do
-		assert true
+	#Test for the post model. We need to ensure name is validated as well as title.
+	#Title must be at least 5 characters
+
+	"Testing Completed:
+		1) No Title - Completed
+		2) Title less than 5 characters - Completed
+		3) Title and greater than 5 characters - Completed"
+	"28.6% Total coverage"
+	test "Validate Post Title" do
+		post = Post.new(:title => "Title is this")
+		assert post.save!
 	end
 end
