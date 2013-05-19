1768a657a77f56ce8d57e32656282f91fe78353a
callumStudent <20530556@student.uwa.edu.au>
2013-05-20 00:40:16 +0800

    Stopped upload of empty photo

--- imageupload/app/models/photo.rb
+++ imageupload/app/models/photo.rb
@@ -4,6 +4,8 @@ class Photo < ActiveRecord::Base
   attr_accessible  :name, :user_id, :image
   belongs_to :user
   has_many :photos
+
+  validates_presence_of :name
   
   mount_uploader :image, ImageUploader
 end
