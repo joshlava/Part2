171fa4f62d91a1e22760e6d1f9ae67d7d40ad2c4
Alistair <gillaw06@gmail.com>
2013-05-21 14:32:02 +0800

    Revert "Revert "moving private_photos to master""
    This reverts commit a08aec43b7ba7d7f4f66ee7f39839707035f1db1.

--- imageupload/app/uploaders/image_uploader.rb
+++ imageupload/app/uploaders/image_uploader.rb
@@ -21,13 +21,12 @@ class ImageUploader < CarrierWave::Uploader::Base
   end
 
   # Provide a default URL as a default if there hasn't been a file uploaded:
-  def default_url
+  # def default_url
   #   # For Rails 3.1+ asset pipeline compatibility:
   #   # asset_path("fallback/" + [version_name, "default.png"].compact.join('_'))
   #
-  "uploads/images/" + [version_name, "default.png"].compact.join('_')
-end
-
+  #   "/images/fallback/" + [version_name, "default.png"].compact.join('_')
+  # end
   version :thumb do
       process :resize_to_fill => [200, 200]
     end
@@ -44,10 +43,6 @@ end
 
   version :medium do
       process :resize_to_limit => [600, 600]
-    end
-    
-    version :feat do
-      process :resize_to_fill => [450, 450]
     end
     
   version :large do
