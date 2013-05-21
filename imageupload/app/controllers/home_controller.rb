78142e249278d62ac451f7bd7044575ecbb6088a
callumStudent <20530556@student.uwa.edu.au>
2013-05-21 14:24:21 +0800

    moving private_photos to master

--- imageupload/app/controllers/home_controller.rb
+++ imageupload/app/controllers/home_controller.rb
@@ -8,10 +8,7 @@ class HomeController < ApplicationController
     @my_user = User.find(:all, :limit => 1, :offset => offset )
     picture_count = Photo.count(params[:user_id => @my_user[0].id])
     poffset = rand(0..(picture_count - 1))
-    if picture_count != 0 
-      @fphoto = Photo.find(:all, :limit => 1, :offset => poffset )
-    else
-      @fphoto =  User.find(:all, :limit => 1, :offset => offset )
-    end
-   end
+    @fphoto = Photo.find(:all, :limit => 1, :offset => poffset )
+    
+  end
 end
