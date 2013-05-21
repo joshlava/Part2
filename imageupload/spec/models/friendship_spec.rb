168d2f1306a49b71b3e7ac110337753d6d12889f
Dylan <nangld01@student.uwa.edu.au>
2013-05-22 00:43:01 +0800

    Finished All Model Tests, Starting on Controller Tests
    We are currently sitting at 80% code coverage for the tests which is
    very good. Should be at 100% tommorow night.

--- imageupload/spec/models/friendship_spec.rb
+++ imageupload/spec/models/friendship_spec.rb
@@ -5,12 +5,12 @@ describe Friendship do
 		FactoryGirl.create(:friendship).should be_valid
 	end
 
-	it "is invalid without a name"
+	it "is valid without a user_id" do
+		FactoryGirl.build(:friendship, user_id: nil).should be_valid
+	end
 
-	it "is invalid without an email"
-
-	it "is invalid without a password" 
-
-	it "when email format is invalid" 
+	it "is valid without a friend_id" do
+		FactoryGirl.build(:friendship, friend_id: nil).should be_valid
+	end
 
 end
--- imageupload/spec/models/roles_spec.rb
+++ imageupload/spec/models/roles_spec.rb
@@ -1,5 +1,7 @@
 require 'spec_helper'
 
-describe Roles do
-  pending "add some examples to (or delete) #{__FILE__}"
+describe Role do
+	it "has a valid factory" do
+		FactoryGirl.create(:role).should be_valid
+	end
 end
