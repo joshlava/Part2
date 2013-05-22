9f19fa9e96494a6b50aeacea1127e284dbb3e09a
Alistair <gillaw06@gmail.com>
2013-05-22 23:21:56 +0800

    CSS

--- imageupload/config/routes.rb
+++ imageupload/config/routes.rb
@@ -14,10 +14,10 @@ Imageupload::Application.routes.draw do
 
   authenticated :user do
     root :to => 'home#index'
-  end
-  devise_scope :user do
-    get "/", :to => "devise/registrations#new"
-  end
+   end
+  #devise_scope :user do
+   # get "/", :to => "devise/registrations#new"
+  #end
   root :to => 'home#index'
   devise_for :users
   resources :users
